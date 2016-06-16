f9d697ee6e735d26e343cdab808215e61e018f77
chooblarin <choo.bla.rin@gmail.com>
2016-06-17 00:32:41 +0900

    Update android gradle plugin 2.1.2

--- sample/src/main/java/com/chooblarin/kusa/demo/MainActivity.java
+++ sample/src/main/java/com/chooblarin/kusa/demo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.chooblarin.kusa.demo;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
